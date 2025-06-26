7f4bcc0ba73c4aa2adf39788a9edaf01f4059b20
AishwaryaP005 <aishwaryarpawar2805@gmail.com>
2025-06-26 11:36:38 +0530

    Add files via upload

---
+++ Diagnocare/Health_record.py
@@ -0,0 +1,30 @@
+ 
+ 
+import streamlit as st 
+ 
+# Page setup - MUST come first 
+st.set_page_config(page_title="Health Insights", layout="wide") 
+if st.button("Back"): 
+    st.switch_page("app.py") 
+ 
+ 
+st.title("Health Insights Dashboard") 
+st.markdown("Explore various insights and visualizations to understand your 
+health data better.") 
+ 
+# Options 
+options = { 
+    "Login/Register": "login" 
+ 
+} 
+ 
+ 
+choices_list = ["Select a module..."] + list(options.keys()) 
+choice = st.selectbox("Choose a health record Module:", choices_list) 
+ 
+if choice == "Select a module...": 
+    st.info("Please select a utility module to continue.") 
+elif options[choice] == "login": 
+    st.switch_page("pages\sapp.py") 
+else: 
+    st.warning("Invalid option.") 
